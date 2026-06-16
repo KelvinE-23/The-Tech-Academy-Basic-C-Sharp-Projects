debec4e2071b5078b814cf0567dba3577f9659a7
KelvinE-23 <essilfiek_22@hotmail.com>
2026-06-16 13:12:01 -0400

    committing StudentDatabase_ConsoleApp

---
+++ StudentDatabase_ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StudentDatabase_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
---
+++ StudentDatabase_ConsoleApp/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace StudentDatabase_ConsoleApp
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext()
+        {
+
+        }
+        // Entities        
+        public DbSet<Student> Students { get; set; }
+        //public DbSet<DateOfBirth> DateOfBirth { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
